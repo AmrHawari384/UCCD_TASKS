b94d8b84e77d1d684b08ab5d72ff05d45f926505
AmrHawari384 <amrhani384@gmail.com>
2022-03-28 22:54:09 +0200

    Added day 1

---
+++ Day1/labs/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+printf("My name is Amr hani\n");
+printf("My birth date is 1 jan 2002\n");
+
+printf("I Graduated from faculity of engineering\n");
+printf("Mansoura Univeristy");
+
+
+
+   return 0;
+}
