b7f5983160fbab1348e64da3b481c300c14f6941
AmrHawari384 <amrhani384@gmail.com>
2022-03-28 22:30:46 +0200

    Added Day 1

---
+++ Day1/assi/3 num reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int x,y,z;
+
+    printf("Enter 1:");
+    scanf("%d",&x);
+    printf("Enter 2:");
+    scanf("%d",&y);
+    printf("Enter 3:");
+    scanf("%d",&z);
+    printf("%d %d %d ",z,y,x);
+    return 0;
+}
