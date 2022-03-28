7f1ffbc37d7e9530a4ac97e9ad00d76bd2313762
AmrHawari384 <amrhani384@gmail.com>
2022-03-28 22:46:34 +0200

    Added day 1 labs

---
+++ Day1/labs/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{int x,y,sum,mul;
+printf("Enter the value of x=");
+scanf("%d",&x);
+printf("Enter the value of y=");
+scanf("%d",&y);
+sum=x+y;
+mul=x*y;
+printf("The summition      of x,y is =%d \n",sum);
+printf("The Multiplication of x,y is =%d",mul);
+	
+}
